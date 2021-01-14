5bf0d78e99561d881754f238c28f5ad205fb1fea
sr <7287663@qq.com>
2021-01-14 17:01:49 +0800

    修改超时时长

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,13 +3,13 @@ import URL from './utils'
 const token = "Bearer".concat(JSON.parse(localStorage.getItem("token")));
 const request = axios.create({
   baseURL: URL.BASE_URL,
-  timeout: 5000 // 请求超时
+  timeout: 10000 // 请求超时
 });
 
 const request1 = axios.create({
   baseURL: URL.BASE_URL,
   headers: { Authorization: token, "Content-Type": "application/json" },
-  timeout: 5000
+  timeout: 10000
 });
 
 export default {
